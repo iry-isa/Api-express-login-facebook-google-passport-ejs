9f18d85bc70b615b7893c107cbacead650319165
Iry-Isa <iryisa@gmail.com>
2020-06-15 12:06:45 +0200

    Api-express-login-facebook-google-passport-ejs

---
+++ README.md
@@ -0,0 +1,4 @@
+Login con Facebook y local con passport en express y mongoDB.
+Motor plantilla ejs.
+
+npm install y npm run nodemon
---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'facebookAuth': {
+        'clientID': 'aqui va la id de cliente',
+        'clientSecret': 'aqui la secret key de cliente',
+        'callbackURL': 'aqui la callback url'
+    },
+
+    'googleAuth': {
+        'clientID': 'aqui va la id de cliente',
+        'clientSecret': 'aqui la secret key de cliente',
+        'callbackURL': 'aqui la callback url'
+    }
+}
